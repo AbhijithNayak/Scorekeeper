7e4f7ee4cadc23c93355f244510f43840a81c34b
Abhijith Nayak <28thstar@gmail.com>
2014-08-23 18:23:03 +0530

    version 0.2 -- UI player add functionality completed

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivy.properties import ObjectProperty
+from kivy.uix.scrollview import ScrollView
+from kivy.properties import ObjectProperty, NumericProperty
 from string import uppercase, upper
+import re
 
 class CapitalInput(TextInput):
     def __init__(self, **kwargs):
@@ -23,23 +25,43 @@ class CapitalInput(TextInput):
         if s in uppercase[:]:
             return super(CapitalInput, self).insert_text(s, from_undo=from_undo)
 
+class IntegerInput(TextInput):
+    def insert_text(self, substring, from_undo=False):
+        p = re.compile('[^0-9]')
+        s = re.sub(p,'',substring)
+        return super(IntegerInput, self).insert_text(s, from_undo=from_undo)
+
 class UserLayout(BoxLayout):
+    name = ObjectProperty(None)
+    score = ObjectProperty(None)
+    total = ObjectProperty(None)
     pass
 
 class ScorekeepWidget(Widget):
 
     databox = ObjectProperty(None)
+    scrollview = ObjectProperty(None)
 
     def add_player(self):
-        self.databox.add_widget(UserLayout())
-        print self.databox.size
-        print self.size
+        ul = UserLayout(height = 0.16*self.scrollview.height)
+        self.databox.add_widget(ul)
+
+        try:
+            self.databox.height = max(ul.height, (len(self.databox.children)*(self.databox.children[0]).height*1.0))
+            #self.databox.pos = [0, 1*self.scrollview.top]
+        except IndexError:
+            print "IE PASS\n"
+            pass
+
        
 
 class ScorekeepApp(App):
     def build(self):
-        wid = ScorekeepWidget()
-        return wid
+        #wid = ScorekeepWidget()
+        #return wid
+        print "ROOT SIZE:\t"
+        print self.root.size
+        pass
 
 
 if __name__ == "__main__":
